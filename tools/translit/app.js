914eca9792a791caa296209fa203c8a3db2dbee9
Yaroslav Serhieiev <yaroslavs@wix.com>
2019-10-06 18:47:16 +0300

    fix(translit): case-sensivity

--- tools/translit/app.js
+++ tools/translit/app.js
@@ -1,174 +1,32 @@
-/* global ta7uul, jsyaml, source, uploadMapping, selectMapping, textTransliterated, placeholderTransliterated */
+/* global jsyaml, source, uploadMapping, selectMapping, textTransliterated, placeholderTransliterated */
 
 let fadeTimer;
-let mappings = { Direct: {} };
+let mappings = { Direct: { maxLength: 0, map: new Map() } };
 
 const PERSISTANCE = {
   YAML: 'transliterator__yaml',
   SELECTION: 'transliterator__selection'
 };
 
-function remap(str, table) {
-  const C = Math.max(1, ...Object.keys(table).map(k => k.length));
-
-  let tra = "";
-  for (let i = 0; i < str.length; i++) {
-    for (let l = C; l >= 0; l--) {
-      if (l === 0) {
-        tra += table[''] || str[i];
-        break;
-      }
-      const chunk = str.substr(i, l);
-      const traChunk = findChunk(chunk, table);
-      if (traChunk !== void 0) {
-        tra += traChunk;
-        i += l - 1;
-        break;
-      }
-    }
-  }
-  return tra;
-}
-
-function findChunk(chunk, table) {
-  if (table.hasOwnProperty(chunk)) {
-    return table[chunk];
-  }
-
-  if (isUpperCase(chunk)) {
-    const uChunk = chunk.toLocaleUpperCase();
-    if (table.hasOwnProperty(uChunk)) {
-      return table[uChunk].toLocaleUpperCase();
-    }
-  }
-
-  if (isLowerCase(chunk)) {
-    const lChunk = chunk.toLocaleLowerCase();
-    if (table.hasOwnProperty(lChunk)) {
-      return table[lChunk].toLocaleLowerCase();
-    }
-  }
-
-  return void 0;
-}
-
-function isUpperCase(str) {
-  const firstChar = str[0];
-  return firstChar.toLocaleUpperCase() === firstChar;
-}
-
-function isLowerCase(str) {
-  const firstChar = str[0];
-  return firstChar.toLocaleLowerCase() === firstChar;
-}
-
-function retransliterate() {
-  const table = mappings[selectMapping.value];
-  if (table) {
-    textTransliterated.textContent = remap(source.value, table);
-  } else {
-    textTransliterated.textContent = source.value;
-  }
-}
-
-function onChangeMapping() {
-  textTransliterated.style.opacity = 0;
-  if (fadeTimer) clearTimeout(fadeTimer);
-  fadeTimer = setTimeout(function () {
-    retransliterate();
-    textTransliterated.style.opacity = 1;
-    fadeTimer = null;
-  }, 150);
+function loadFromStorage() {
+  const [yaml, selection] = [
+    localStorage.getItem(PERSISTANCE.YAML),
+    localStorage.getItem(PERSISTANCE.SELECTION)
+  ];
 
   try {
-    localStorage.setItem(PERSISTANCE.SELECTION, selectMapping.value);
+    const newMappings = yaml ? parseYaml(yaml) : void 0;
+    populateMappings(newMappings);
   } catch (e) {
-    console.error('Save selection error', e);
+    console.error('Load settings error', e);
+  }
+
+  if (mappings[selection]) {
+    selectMapping.value = selection;
   }
 }
 
-function populateMappings(newMappings = mappings) {
-  mappings = newMappings;
-
-  selectMapping.innerHTML = '';
-  const allRules = Object.keys(mappings);
-  allRules.forEach(function (ruleName) {
-    const opt = document.createElement("option");
-    opt.value = ruleName;
-    opt.textContent = ruleName;
-    selectMapping.appendChild(opt);
-  });
-
-  onChangeMapping();
-}
-
-function checkEnter(ev) {
-  if (ev.which !== 13)
-    return;
-
-  ev.preventDefault();
-  source.blur();
-
-  selectTransliteratedContents();
-}
-
-function selectTransliteratedContents() {
-  const range = document.createRange();
-  range.selectNodeContents(textTransliterated);
-
-  const sel = window.getSelection();
-  sel.removeAllRanges();
-  sel.addRange(range);
-}
-
-function byLength(a, b) {
-  if (a.length > b.length) return -1;
-  if (a.length < b.length) return 1;
-  return a.localeCompare(b);
-}
-
-function parseYaml(str) {
-  let parsed;
-
-  try {
-    parsed = jsyaml.safeLoad(str);
-  } catch (e) {
-    throw new Error(`File parsing error:\n${e.message}`);
-  }
-
-  try {
-    const newMappings = {};
-
-    for (const entry of Object.entries(parsed)) {
-      const [schema, table] = entry;
-      const resorted = {};
-      const keys = Object.keys(table).sort(byLength);
-
-      for (const key of keys) {
-        const lowerKey = key.toLocaleLowerCase();
-        const upperKey = key.toLocaleUpperCase();
-        const value = table[key];
-
-        resorted[key] = value;
-        if (!table.hasOwnProperty(lowerKey)) {
-          resorted[lowerKey] = value.toLocaleLowerCase();
-        }
-
-        if (!table.hasOwnProperty(upperKey)) {
-          resorted[upperKey] = value.toLocaleUpperCase();
-        }
-
-        newMappings[schema] = resorted;
-      }
-    }
-
-    return newMappings;
-  } catch (e) {
-    throw new Error(`File interpretation error:\n${e.stack}`);
-  }
-}
-
-function loadMapping(e) {
+function onLoadMapping(e) {
   const [file] = e.target.files;
 
   if (!file) {
@@ -202,6 +60,153 @@ function loadMapping(e) {
   reader.readAsText(file, "utf-8");
 }
 
+function onSelectMapping() {
+  textTransliterated.style.opacity = 0;
+  if (fadeTimer) clearTimeout(fadeTimer);
+  fadeTimer = setTimeout(function () {
+    updateContents();
+    textTransliterated.style.opacity = 1;
+    fadeTimer = null;
+  }, 150);
+
+  try {
+    localStorage.setItem(PERSISTANCE.SELECTION, selectMapping.value);
+  } catch (e) {
+    console.error('Save selection error', e);
+  }
+}
+
+function updateContents() {
+  const currentSchema = mappings[selectMapping.value];
+  const originalText = source.value;
+  const transliteratedText = currentSchema
+      ? remap(originalText, currentSchema)
+      : originalText;
+
+  textTransliterated.textContent = transliteratedText;
+}
+
+function remap(str, { maxLength, map }) {
+  if (map.size === 0) {
+    return str;
+  }
+
+  let result = "";
+  for (let i = 0; i < str.length; i++) {
+    for (let l = maxLength; l >= 0; l--) {
+      if (l > 0) {
+        const chunk = str.substr(i, l);
+        const transliteratedChunk = findChunk(chunk, map);
+        if (transliteratedChunk !== void 0) {
+          result += transliteratedChunk;
+          i += l - 1;
+          break;
+        }
+      } else {
+        result += map.get('') || str[i];
+      }
+    }
+  }
+
+  return result;
+}
+
+function findChunk(chunk, table) {
+  if (table.has(chunk)) {
+    return table.get(chunk);
+  }
+
+  const fallbackChunk = isLowerCase(chunk, 0)
+    ? chunk.toLocaleLowerCase()
+    : chunk.toLocaleUpperCase();
+
+  if (table.has(fallbackChunk)) {
+    return mimicCase(table.get(fallbackChunk), chunk);
+  }
+
+  return void 0;
+}
+
+function mimicCase(str, pattern) {
+  let res = '';
+
+  const upper = str.toLocaleUpperCase();
+  const lower = str.toLocaleLowerCase();
+
+  for (let i = 0; i < str.length; i++) {
+    res += (isLowerCase(pattern, i) ? lower : upper)[i];
+  }
+
+  return res;
+}
+
+function isLowerCase(str, index = 0) {
+  const chr = str[index];
+  return chr.toLocaleLowerCase() === chr;
+}
+
+function populateMappings(newMappings = mappings) {
+  mappings = newMappings;
+
+  selectMapping.innerHTML = '';
+  const allRules = Object.keys(mappings);
+  allRules.forEach(function (ruleName) {
+    const opt = document.createElement("option");
+    opt.value = ruleName;
+    opt.textContent = ruleName;
+    selectMapping.appendChild(opt);
+  });
+
+  onSelectMapping();
+}
+
+function parseYaml(str) {
+  let parsed;
+
+  try {
+    parsed = jsyaml.safeLoad(str);
+  } catch (e) {
+    throw new Error(`File parsing error:\n${e.message}`);
+  }
+
+  try {
+    const schemas = {};
+
+    for (const entry of Object.entries(parsed)) {
+      const [name, dictionary] = entry;
+      const map = new Map();
+      let maxLength = 1;
+
+      for (const key of Object.keys(dictionary)) {
+        const value = dictionary[key];
+
+        maxLength = Math.max(value.length, maxLength);
+        map.set(key, value);
+
+        const lowerKey = key.toLocaleLowerCase();
+        if (!dictionary.hasOwnProperty(lowerKey)) {
+          map.set(lowerKey, value.toLocaleLowerCase());
+        }
+
+        const upperKey = key.toLocaleUpperCase();
+        if (!dictionary.hasOwnProperty(upperKey)) {
+          map.set(upperKey, value.toLocaleUpperCase());
+        }
+
+        schemas[name] = {
+          maxLength,
+          map: map,
+        };
+      }
+    }
+
+    return schemas;
+  } catch (e) {
+    throw new Error(`File interpretation error:\n${e.stack}`);
+  }
+}
+
+
 function saveFile(yaml) {
   try {
     localStorage.setItem(PERSISTANCE.YAML, yaml)
@@ -210,29 +215,11 @@ function saveFile(yaml) {
   }
 }
 
-function loadFromStorage() {
-  const [yaml, selection] = [
-    localStorage.getItem(PERSISTANCE.YAML),
-    localStorage.getItem(PERSISTANCE.SELECTION)
-  ];
-
-  try {
-    const newMappings = yaml ? parseYaml(yaml) : void 0;
-    populateMappings(newMappings);
-  } catch (e) {
-    console.error('Load settings error', e);
-  }
-
-  if (mappings[selection]) {
-    selectMapping.value = selection;
-  }
-}
-
 window.onload = function () {
   loadFromStorage();
 
-  uploadMapping.onchange = loadMapping;
-  selectMapping.onchange = onChangeMapping;
-  source.oninput = retransliterate;
-  textTransliterated.onkeyup = checkEnter;
+  uploadMapping.onchange = onLoadMapping;
+  selectMapping.onchange = onSelectMapping;
+  source.oninput = updateContents;
+  textTransliterated.onclick = textTransliterated.onfocus = () => textTransliterated.select();
 };
