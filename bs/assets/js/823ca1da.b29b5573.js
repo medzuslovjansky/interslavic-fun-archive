e07d9babe7e299995d2b088be0c1269583ed8c56
Yaroslav Serhieiev <noomorph@gmail.com>
2023-07-17 20:06:46 +0300

    Deploy website - based on 9236925e84ca57c49c4e6131cf80289c2ab9603c

---
+++ bs/assets/js/823ca1da.b29b5573.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkinterslavic_fun=self.webpackChunkinterslavic_fun||[]).push([[3174],{4469:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-blog","id":"default"}')}}]);
---
+++ pl/assets/js/75d7807b.77060d8e.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkinterslavic_fun=self.webpackChunkinterslavic_fun||[]).push([[6431],{3769:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}')}}]);
